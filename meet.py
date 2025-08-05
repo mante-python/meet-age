4af500f9e8ed854d0be398f6e3c83cd12353f7ff
Panos <panostutoring94@gmail.com>
2025-08-05 10:45:07 +0300

    Add files via upload
    This is my first attempt at a program that asks for your first and last name, age, and gender.

---
+++ meet.py
@@ -0,0 +1,8 @@
+name = input("What's your name?")
+age = input("How old are you?")
+sex = input("What's your sex?")
+if sex == "male":
+    print("Hello Mr.", name, "You are just", age, "years old")
+elif sex == "female":
+    print("Hello Mrs.", name, "You are just", age, "years old")
+print("Welcome to Greece, nice to meet you!")
